ebbff6d26daedfc89be307b05dbaf58e6b687160
Ethan O'Donnell <ethan@ejpo.io>
2021-03-03 06:07:27 +0000

    final changes before testing

--- etc/downloadconfig.py
+++ etc/downloadconfig.py
@@ -14,26 +14,30 @@ def main() :
     #Get latest release
     #From release get zipfile that matches thie CONFIG env var passed to this container
     #Clear the env var that stores the key
-    print(authHeader)
     latestRelease = getWithBasicAuth("{0}/repos/{1}/{2}/releases/latest".format(baseURI,ghUser,ghRepo),authHeader)
     releaseJson = json.load(latestRelease)
     
     print(os.getcwd())
+
     for asset in releaseJson['assets'] :
         if asset['name'] == (containerName + ".zip") :
+            print("Found Matching Asset")
             configArchiveStream = downloadConfig(asset['url'], authHeader)
             f = open("{0}/{1}.zip".format(os.getcwd(),containerName),'w+b')
             f.write(configArchiveStream.read())
+            print("Zip File Dropped")
 
 def getWithBasicAuth (requestUri, headerToken) :
     headers = { 'Authorization' : ('basic ' + headerToken)}
     thisRequest = req.Request(url=requestUri, headers=headers)
+    print("Getting Latest Release")
     return req.urlopen(thisRequest)
 
 def downloadConfig (requestUri, headerToken) :
     headers = { 'Authorization' : ('basic ' + headerToken),
                 'Accept': 'application/octet-stream'}
     thisRequest = req.Request(url=requestUri, headers=headers)
+    print("Downloading config")
     return req.urlopen(thisRequest)
 
 
--- etc/entry.sh
+++ etc/entry.sh
@@ -4,6 +4,14 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+app_update "${STEAMAPPID}" \
 				+quit
 
+bash rm -rf ${STEAMAPPDIR}/SquadGame/ServerConfig/*
+
+python3 etc/downloadconfig.py
+
+unzip "${CONTAINER_NAME}.zip"
+
+bash mv ${CONTAINER_NAME}/* ${STEAMAPPDIR}/SquadGame/ServerConfig/.
+
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
 sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SquadGame/ServerConfig/Rcon.cfg"
 
