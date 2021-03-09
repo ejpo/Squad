0e8193a50036542c315edd5e2b64bd1065c6704e
Ethan O'Donnell <ejpo2@kent.ac.uk>
2021-03-09 19:15:13 +0000

    changed debug task

--- etc/entry.sh
+++ etc/entry.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin
+
 bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+force_install_dir "${STEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
 				+quit
 
-bash rm -rf ${STEAMAPPDIR}/SquadGame/ServerConfig/*
+rm -rf ${STEAMAPPDIR}/SquadGame/ServerConfig/*
 
 python3 downloadconfig.py
 
 unzip "${CONTAINER_NAME}.zip"
 
-bash mv ${CONTAINER_NAME}/* ${STEAMAPPDIR}/SquadGame/ServerConfig/.
+mv ${CONTAINER_NAME}/* ${STEAMAPPDIR}/SquadGame/ServerConfig/.
 
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
 sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SquadGame/ServerConfig/Rcon.cfg"
 
-bash sleep 3h
+bash end=$((SECONDS+3600)) \
+while [ $SECONDS -lt $end ]; do \
+    echo $SECONDS \
+    : \
+done
 
 #bash "${STEAMAPPDIR}/SquadGameServer.sh" \
 #			Port="${PORT}" \
