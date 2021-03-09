825bff6d2a6f1292fa9ee7ac68dfc6f58281d770
Ethan O'Donnell <ejpo2@kent.ac.uk>
2021-03-09 18:39:48 +0000

    debug changes

--- etc/entry.sh
+++ etc/entry.sh
@@ -15,9 +15,11 @@ bash mv ${CONTAINER_NAME}/* ${STEAMAPPDIR}/SquadGame/ServerConfig/.
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
 sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SquadGame/ServerConfig/Rcon.cfg"
 
-bash "${STEAMAPPDIR}/SquadGameServer.sh" \
-			Port="${PORT}" \
-			QueryPort="${QUERYPORT}" \
-			RCONPORT="${RCONPORT}" \
-			FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" \
-			FIXEDMAXTICKRATE="${FIXEDMAXTICKRATE}"
+bash sleep 3h
+
+#bash "${STEAMAPPDIR}/SquadGameServer.sh" \
+#			Port="${PORT}" \
+#			QueryPort="${QUERYPORT}" \
+#			RCONPORT="${RCONPORT}" \
+#			FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" \
+#			FIXEDMAXTICKRATE="${FIXEDMAXTICKRATE}"
